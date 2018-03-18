38eb8e42a0af6d1699cdc1e136c112022bcf16f4
Matt Chun-Lum <mchunlum@gmail.com>
2018-03-17 23:20:47 -0700

    remove dubug builds

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run -bin bb --target $TARGET help
-    cross run -bin bb --target $TARGET --release help
+    cross run --bin bb --target $TARGET --release help
 }
 
 # we don't run the "test phase" when doing deploys
